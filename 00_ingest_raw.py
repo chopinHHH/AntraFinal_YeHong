ce8bd803d3d33b929bc0e8f67f69f6bf8838cd4d
chopinHHH <chopinh0629@gmail.com>
2021-12-20 22:16:25 -0500

    Ingest raw data & Raw to Bronze
    Fully completed raw data ingestion and started to initial Raw to Bronze process.

--- 00_ingest_raw.py
+++ 00_ingest_raw.py
@@ -4,12 +4,8 @@
 
 # COMMAND ----------
 
-# MAGIC %run ./includes/configuration
-
-# COMMAND ----------
-
-# Utility Function
-%run ./includes/utilities
+# Step Configuration
+%run ./includes/configuration
 
 # COMMAND ----------
 
@@ -18,17 +14,80 @@ dbutils.fs.rm(commonPath, recurse=True)
 
 # COMMAND ----------
 
-# Show rawData path
-display([dbutils.fs.ls(path) for path in rawData])
+# Show rawData Path
+display(dbutils.fs.ls(rawPath))
 
 # COMMAND ----------
 
-
+# Read and cache 8 json files
+rawDF = (spark
+            .read
+            .format("json")
+            .option("multiline", "true")
+            .load(rawData)
+            .cache()
+           )
 
 # COMMAND ----------
 
-# Ingest raw data
-from pyspark.sql.functions import *
+# print schema of rawDF
+rawDF.printSchema()
 
-df = spark.read.json(rawData)
-display(df)
+# COMMAND ----------
+
+#Print the Contents of the Raw Files
+print(dbutils.fs.head(dbutils.fs.ls(rawPath)[0].path))
+
+# COMMAND ----------
+
+# Define column name
+movie_schema = ["BackdropUrl", "Budget", "CreatedBy", "CreatedDate", "Id", "ImdbUrl", "OriginalLanguage", "Overview", "PosterUrl", "Price", "ReleaseDate", "Revenue", "RunTime", "Tagline", "Title", "TmdbUrl", "UpdatedBy", "UpdatedDate", "genres"]
+
+for i in range(len(movie_schema)):
+    movie_schema[i] = "movie." + movie_schema[i]
+
+# COMMAND ----------
+
+# Read columns
+movieInitialDF = rawDF.select(
+    array(expr("movie.BackdropUrl")).alias("BackdropUrl"),
+    array(expr("movie.Budget")).alias("Budget"),
+    array(expr("movie.CreatedBy")).alias("CreatedBy"),
+    array(expr("movie.CreatedDate")).alias("CreatedDate"),
+    array(expr("movie.Id")).alias("Id"),
+    array(expr("movie.ImdbUrl")).alias("ImdbUrl"),
+    array(expr("movie.OriginalLanguage")).alias("OriginalLanguage"),
+    array(expr("movie.Overview")).alias("Overview"),
+    array(expr("movie.PosterUrl")).alias("PosterUrl"),
+    array(expr("movie.Price")).alias("Price"),
+    array(expr("movie.ReleaseDate")).alias("ReleaseDate"),
+    array(expr("movie.Revenue")).alias("Revenue"),
+    array(expr("movie.RunTime")).alias("RunTime"),
+    array(expr("movie.Tagline")).alias("Tagline"),
+    array(expr("movie.Title")).alias("Title"),
+    array(expr("movie.TmdbUrl")).alias("TmdbUrl"),
+    array(expr("movie.UpdatedBy")).alias("UpdatedBy"),
+    array(expr("movie.UpdatedDate")).alias("UpdatedDate"),
+    array(expr("movie.genres")).alias("Genres")
+)
+
+movieInitialDF.show()
+
+# COMMAND ----------
+
+# Combine Columns
+ms_DF = (movieInitialDF.withColumn("movie", explode(arrays_zip("BackdropUrl", "Budget", "CreatedBy", "CreatedDate", "Id", "ImdbUrl", "OriginalLanguage", "Overview", "PosterUrl", "Price", "ReleaseDate", "Revenue", "RunTime", "Tagline", "Title", "TmdbUrl", "UpdatedBy", "UpdatedDate", "genres")))
+.select('movie.BackdropUrl', 'movie.Budget', 'movie.CreatedBy', 'movie.CreatedDate', 'movie.Id', 'movie.ImdbUrl', 'movie.OriginalLanguage', 'movie.Overview', 'movie.PosterUrl', 'movie.Price', 'movie.ReleaseDate', 'movie.Revenue', 'movie.RunTime', 'movie.Tagline', 'movie.Title', 'movie.TmdbUrl', 'movie.UpdatedBy', 'movie.UpdatedDate', 'movie.genres'))
+
+movieDF = (ms_DF.withColumn("movie", explode(arrays_zip("BackdropUrl", "Budget", "CreatedBy", "CreatedDate", "Id", "ImdbUrl", "OriginalLanguage", "Overview", "PosterUrl", "Price", "ReleaseDate", "Revenue", "RunTime", "Tagline", "Title", "TmdbUrl", "UpdatedBy", "UpdatedDate", "genres")))
+.select('movie.BackdropUrl', 'movie.Budget', 'movie.CreatedBy', 'movie.CreatedDate', 'movie.Id', 'movie.ImdbUrl', 'movie.OriginalLanguage', 'movie.Overview', 'movie.PosterUrl', 'movie.Price', 'movie.ReleaseDate', 'movie.Revenue', 'movie.RunTime', 'movie.Tagline', 'movie.Title', 'movie.TmdbUrl', 'movie.UpdatedBy', 'movie.UpdatedDate', 'movie.genres'))
+
+# COMMAND ----------
+
+#Display movie dataframe
+display(movieDF)
+
+# COMMAND ----------
+
+#Testing
+# movie_id.withColumn("movie", explode(arrays_zip("Id"))).select("movie.Id").show()
