e5462d99b5ab01dd72475cf4c343ee731b7f424a
chopinHHH <chopinh0629@gmail.com>
2021-12-22 00:13:11 -0500

    Update configuration

---
+++ includes/configuration.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ###Define data path
+
+# COMMAND ----------
+
+commonPath = f"/mnt/movieShop/"
+
+rawPath = commonPath + "raw/"
+rawData = [file.path for file in dbutils.fs.ls("dbfs:"+rawPath)]
+bronzePath = commonPath + "bronze/"
+silverPath = commonPath + "silver/"
+silverQuarantinePath = commonPath + "silverQuarantine/"
